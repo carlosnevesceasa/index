0985c2c0e5cc8e5fea972a771912d5835235fd8d
carlosnevesceasa <carlos.neves.ceasa@gmail.com>
2025-09-22 10:44:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ceasa-cache-v16";
+const CACHE_NAME = "ceasa-cache-v17";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -38,6 +38,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
